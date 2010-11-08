69f6b178494d7bd5c4baee8617157a2fae35d16a
Chao Wang <chaow@pressglass.corp.yahoo.com>
2010-11-08 10:46:05 -0800

    my changes

---
+++ core/src/main/java/org/apache/oozie/command/wf/XWorkflowActionInfoCommand.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2010 Yahoo! Inc. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. See accompanying LICENSE file.
+ */
+package org.apache.oozie.command.wf;
+
+
+import org.apache.oozie.ErrorCode;
+import org.apache.oozie.WorkflowActionBean;
+import org.apache.oozie.command.CommandException;
+import org.apache.oozie.command.jpa.WorkflowActionGetCommand;
+import org.apache.oozie.util.ParamChecker;
+import org.apache.oozie.util.XLog;
+import org.apache.oozie.service.JPAService;
+import org.apache.oozie.service.Services;
+
+public class XWorkflowActionInfoCommand extends XWorkflowCommand<WorkflowActionBean> {
+    private String id;
+    private static XLog LOG = XLog.getLog(XWorkflowActionInfoCommand.class);
+    
+    /**
+     * @param id
+     */
+    public XWorkflowActionInfoCommand(String id) {
+        super("action.info", "action.info", 1);
+        this.id = ParamChecker.notEmpty(id, "id");
+        getLog().debug("Command for workflow action " + id);
+    }
+
+    @Override
+    protected WorkflowActionBean execute() throws CommandException {        
+        JPAService jpaService = Services.get().get(JPAService.class);
+        if (jpaService != null) {
+            WorkflowActionBean action = jpaService.execute(new WorkflowActionGetCommand(this.id));
+            return action;
+        }
+        else {
+            getLog().error(ErrorCode.E0610);
+            return null;
+        }
+    }
+ 
+    /* (non-Javadoc)
+     * @see org.apache.oozie.command.XCommand#getLog()
+     */
+    @Override
+    protected XLog getLog() {
+        return LOG;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.oozie.command.XCommand#getEntityKey()
+     */
+    @Override
+    protected String getEntityKey() {
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.oozie.command.XCommand#loadState()
+     */
+    @Override
+    protected void loadState() {
+        
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.oozie.command.XCommand#verifyPrecondition()
+     */
+    @Override
+    protected void verifyPrecondition() throws CommandException {
+        
+    }
+    
+}
