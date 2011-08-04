2464c32d2940ca4ef99969c8459ca419f64036ce
Chao Wang <wangch@us.ibm.com>
2011-08-03 17:50:39 -0700

    added progress info to coordinator job

--- client/src/main/java/org/apache/oozie/client/CoordinatorJob.java
+++ client/src/main/java/org/apache/oozie/client/CoordinatorJob.java
@@ -182,4 +182,9 @@ public interface CoordinatorJob {
      * @return the list of coordinator actions.
      */
     List<CoordinatorAction> getActions();
+    
+    /**
+     * Return the workflow job progress in %.
+     */
+    float getProgress();
 }
